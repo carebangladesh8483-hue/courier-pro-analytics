6d13f82dc39510cec51f43a6c138fdf92d53e838
carebangladesh8483-hue <carebangladesh8483@gmail.com>
2025-10-21 01:08:33 +0600

    Create Header.tsx

---
+++ components/layout/Header.tsx
@@ -0,0 +1,8 @@
+// components/layout/Header.tsx
+export default function Header() {
+  return (
+    <header className="p-4 bg-white border-b border-gray-200">
+      <h1 className="text-xl font-bold">Courier Pro Analytics</h1>
+    </header>
+  );
+}
