31b1bb4291ae7e09d7edccc1ab0dd199458c24bc
carebangladesh8483-hue <carebangladesh8483@gmail.com>
2025-10-21 09:49:08 +0600

    Create Settings.tsx

---
+++ components/pages/Settings.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Settings() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Settings Page</h1>
+      <p>This is a placeholder Settings page.</p>
+    </div>
+  );
+}
